1769536ae65a831fed6bfeaf2961c60406a2f2d8
MoraPresence <od34156la@mail.ru>
2020-02-03 18:28:47 +0300

    version 0.0.1

--- include/header.hpp
+++ include/header.hpp
@@ -3,4 +3,44 @@
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
 
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+#include "picosha2.h"
+#include <future>
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <boost/log/trivial.hpp>
+
+class mythreads {
+public:
+    mythreads() = default;
+
+    explicit mythreads(std::size_t threadCount) {
+        _threadCount = threadCount;
+    }
+
+
+    static std::string get_hash();
+
+    static bool checkHash(const std::string &);
+
+    static void startLogging();
+
+    static void makeHash();
+
+    static std::size_t _threadCount;
+    static std::string correctEND;
+    static std::atomic_uint infoCount;
+    static std::size_t currentCount;
+    static std::mutex mute;
+};
+
+std::string mythreads::correctEND = "0000";
+std::size_t mythreads::_threadCount = std::thread::hardware_concurrency();
+std::atomic_uint mythreads::infoCount = 0;
+std::size_t mythreads::currentCount = 0;
+std::mutex mythreads::mute;
+
 #endif // INCLUDE_HEADER_HPP_
--- sources/source.cpp
+++ sources/source.cpp
@@ -1,3 +1,55 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <header.hpp>
+std::string mythreads::get_hash() {
+    std::string randomString = std::to_string(std::rand());
+    const std::string hash = picosha2::hash256_hex_string(randomString);
+    return hash;
+}
+
+bool mythreads::checkHash(const std::string &hash) {
+    return hash.substr(hash.size() - correctEND.size()) == correctEND;
+}
+
+
+void mythreads::makeHash() {
+    ++mythreads::currentCount;
+    while (true) {
+        std::string hash = get_hash();
+        if (checkHash(hash)) {
+            mythreads::mute.lock();
+            BOOST_LOG_TRIVIAL(info)
+                    << "Thread with ID: " << std::this_thread::get_id()
+                    << " found hash " << hash
+                    << "!";
+            mythreads::mute.unlock();
+        } else {
+            BOOST_LOG_TRIVIAL(trace)
+                    << "Thread with ID: " << std::this_thread::get_id()
+                    << " with hash " << hashOfRandString
+                    << "!";
+        }
+    }
+}
+
+
+void mythreads::startLogging() {
+    if (_threadCount >= 1 && _threadCount <= std::thread::hardware_concurrency()) {
+        std::vector<std::thread> threads;
+        threads.reserve(_threadCount);
+        std::thread thr(mythreads::makeHash);
+        std::thread thr1(mythreads::makeHash);
+        std::thread thr2(mythreads::makeHash);
+
+        thr.join();
+        thr1.join();
+        thr2.join();
+    } else return;
+}
+
+int main() {
+    ++mythreads::currentCount;
+    mythreads ths(8);
+    ths.startLogging();
+    return 0;
+}
