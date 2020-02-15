f7e8302d2fed521587b2307af859cc54f9439695
MoraPresence <od34156la@mail.ru>
2020-02-15 16:04:00 +0300

    version 0.1.5

--- include/header.hpp
+++ include/header.hpp
@@ -12,6 +12,16 @@
 #include <vector>
 #include <mutex>
 #include <boost/log/trivial.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/support/date_time.hpp>
+#include <boost/log/sources/severity_channel_logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/utility/setup/console.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/sinks.hpp>
+#include <ctime>
+#include <cstdlib>
 
 class mythreads {
 public:
@@ -21,7 +31,6 @@ public:
         _threadCount = threadCount;
     }
 
-
     static std::string get_hash();
 
     static bool checkHash(const std::string &);
@@ -29,6 +38,12 @@ public:
     static void startLogging();
 
     static void makeHash();
+
+    static void logInfo(const std::string &);
+
+    static void logTrace(const std::string &);
+
+    static void logInFile();
 
     static std::size_t _threadCount;
     static std::string correctEND;
@@ -42,5 +57,4 @@ std::size_t mythreads::_threadCount = std::thread::hardware_concurrency();
 std::atomic_uint mythreads::infoCount = 0;
 std::size_t mythreads::currentCount = 0;
 std::mutex mythreads::mute;
-
 #endif // INCLUDE_HEADER_HPP_
--- sources/source.cpp
+++ sources/source.cpp
@@ -2,10 +2,12 @@
 
 #include <header.hpp>
 std::string mythreads::get_hash() {
-    std::string randomString = std::to_string(std::rand());
+    static unsigned int rand = time(nullptr);
+    std::string randomString = std::to_string(rand_r(&rand));
     const std::string hash = picosha2::hash256_hex_string(randomString);
     return hash;
 }
+
 
 bool mythreads::checkHash(const std::string &hash) {
     return hash.substr(hash.size() - correctEND.size()) == correctEND;
@@ -14,44 +16,84 @@ bool mythreads::checkHash(const std::string &hash) {
 
 void mythreads::makeHash() {
     ++mythreads::currentCount;
-    while (true) {
+    while (infoCount < 10) {
+        mythreads::mute.lock();
         std::string hash = get_hash();
+        mythreads::mute.unlock();
+
         if (checkHash(hash)) {
-            mythreads::mute.lock();
-            BOOST_LOG_TRIVIAL(info)
-                    << "Thread with ID: " << std::this_thread::get_id()
-                    << " found hash " << hash
-                    << "!";
-            mythreads::mute.unlock();
+            logInfo(hash);
+            infoCount++;
         } else {
-            BOOST_LOG_TRIVIAL(trace)
-                    << "Thread with ID: " << std::this_thread::get_id()
-                    << " with hash " << hashOfRandString
-                    << "!";
+            logTrace(hash);
         }
     }
 }
 
 
 void mythreads::startLogging() {
-    if (_threadCount >= 1 && 
-	_threadCount <= std::thread::hardware_concurrency()) {
+    if (_threadCount >= 1 && _threadCount <= std::thread::hardware_concurrency()) {
         std::vector<std::thread> threads;
         threads.reserve(_threadCount);
-        std::thread thr(mythreads::makeHash);
-        std::thread thr1(mythreads::makeHash);
-        std::thread thr2(mythreads::makeHash);
-
-        thr.join();
-        thr1.join();
-        thr2.join();
-    } else 
-		return;
+        for (size_t i = 0; i < _threadCount; ++i) {
+            threads.emplace_back(makeHash);
+        }
+        for (auto &th : threads) {
+            th.join();
+        }
+    } else return;
 }
 
-int main() {
-    ++mythreads::currentCount;
-    mythreads ths(8);
-    ths.startLogging();
-    return 0;
+
+void mythreads::logInfo(const std::string &hash) {
+    BOOST_LOG_TRIVIAL(info) << "Thread with ID: "
+                            << std::this_thread::get_id() << " found hash - "
+                            << hash << std::endl;
+}
+
+
+void mythreads::logTrace(const std::string &hash) {
+    BOOST_LOG_TRIVIAL(trace) << "Thread with ID: "
+                             << std::this_thread::get_id() << " found hash - "
+                             << hash << std::endl;
+}
+
+
+namespace logging = boost::log;
+namespace src = boost::log::sources;
+namespace expr = boost::log::expressions;
+namespace keywords = boost::log::keywords;
+namespace sinks = boost::log::sinks;
+
+
+void mythreads::logInFile() {
+    logging::add_file_log
+            (
+                    keywords::file_name = "/home/mora/Desktop/info.log",
+                    keywords::rotation_size = 256 * 1024 * 1024,
+                    keywords::time_based_rotation = sinks::file::rotation_at_time_point(0, 0, 0),
+                    keywords::filter = logging::trivial::severity >= logging::trivial::info,
+                    keywords::format =
+                            (
+                                    expr::stream
+                                            << boost::posix_time::second_clock::local_time()
+                                            << " : <" << logging::trivial::severity
+                                            << "> " << expr::smessage
+                            )
+            );
+
+    logging::add_file_log
+            (
+                    keywords::file_name = "/home/mora/Desktop/trace.log",
+                    keywords::rotation_size = 256 * 1024 * 1024,
+                    keywords::time_based_rotation = sinks::file::rotation_at_time_point(0, 0, 0),
+                    keywords::filter = logging::trivial::severity >= logging::trivial::trace,
+                    keywords::format =
+                            (
+                                    expr::stream
+                                            << boost::posix_time::second_clock::local_time()
+                                            << " : <" << logging::trivial::severity
+                                            << "> " << expr::smessage
+                            )
+            );
 }
