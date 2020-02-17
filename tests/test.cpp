763112be088b7b9e87b70a2596758f0c97dad8af
MoraPresence <od34156la@mail.ru>
2020-02-17 17:06:38 +0300

    final

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,10 +1,7 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <gtest/gtest.h>
-#include "header.hpp"
+
 TEST(Example, EmptyTest) {
-    ++mythreads::currentCount;
-    mythreads ths(4);
-    ths.startLogging();
     EXPECT_TRUE(true);
 }
